a8fc7e8a5033ea9e5d039e1e55ab818ab2ba006e
ucu072@gmail.com <ucu072@gmail.com@99d4cb7e-916e-4139-59c3-c7d8ebe338cf>
2010-11-14 12:32:04 +0000

    2010-11-14 scores Table 구현 완료

--- trunk/CORA/src/com/zoozoobar/torajim/linkprediction/ObjectData.java
+++ trunk/CORA/src/com/zoozoobar/torajim/linkprediction/ObjectData.java
@@ -2,7 +2,7 @@ package com.zoozoobar.torajim.linkprediction;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.Iterator;
 
 public class ObjectData {
 //	public static Set<ObjectKeyword> KEYWORD_LIST = new HashSet<ObjectKeyword>();
@@ -10,10 +10,16 @@ public class ObjectData {
 	private static HashMap<String, ObjectAuthor> AUTHOR_LIST = new HashMap<String, ObjectAuthor> (400000, (float)0.75);
 //	public static Set<ObjectKeyword> KEYWORD_LIST = new HashSet<ObjectKeyword>();
 	private static HashMap<String, ObjectKeyword> KEYWORD_PAPERCOUNT = new HashMap<String, ObjectKeyword>(400000, (float)0.75);
+	private static HashMap<String, ObjectAuthorKeywords> AUTHOR_KEYWORDS = new HashMap<String, ObjectAuthorKeywords>(400000, (float) 0.75);
 
 	public static void initData() {
 		long start = System.currentTimeMillis();
 		long end;
+		
+		TableDB.initDataAuthorKeywords(AUTHOR_KEYWORDS);
+		end = System.currentTimeMillis();
+		System.out.println("AuthorKeywords:" + (end-start)/1000.0 + "s");
+		
 		TableDB.initDataKeywordPapercount(KEYWORD_PAPERCOUNT);
 		end = System.currentTimeMillis();
 		System.out.println("KeywordPapercount:" + (end-start)/1000.0 + "s");
@@ -26,9 +32,10 @@ public class ObjectData {
 		TableDB.initDataPairList(PAIR_LIST);
 		end = System.currentTimeMillis();
 		System.out.println("PairList" + PAIR_LIST.size() + "\t" + (end - start)/1000.0 + "s");
+		
 	}
 
-	public static Set<ObjectPair> getPAIR_LIST() {
+	public static HashSet<ObjectPair> getPAIR_LIST() {
 		return PAIR_LIST;
 	}
 
@@ -38,5 +45,46 @@ public class ObjectData {
 
 	public static HashMap<String, ObjectKeyword> getKEYWORD_PAPERCOUNT() {
 		return KEYWORD_PAPERCOUNT;
+	}
+	
+	public static HashMap<String, ObjectAuthorKeywords> getAUTHOR_KEYWORDS() {
+		return AUTHOR_KEYWORDS;
+	}
+	
+	public static void printValidset() {
+		Iterator<ObjectPair> iter = PAIR_LIST.iterator();
+		while(iter.hasNext()) {
+			ObjectPair ob = iter.next();
+			ObjectAuthor A = ob.getAuthorA();
+			ObjectAuthor B = ob.getAuthorB();
+//			System.out.print("|" + A.getAuthor() + "|" + A.getCountL() + "|" + A.getCountT());
+//			System.out.println("|" + B.getAuthor() + "|" + B.getCountL() + "|" + B.getCountT());
+			if(A.getCountL() > 2 && A.getCountT() > 2) {
+				System.out.print("|" + A.getAuthor() + "|");
+			}
+			if(B.getCountL() >2 && B.getCountT() > 2) {
+				System.out.println("|" + B.getAuthor() + "|");
+			}
+		}
+	}
+	
+	public static void printTest() {
+		Iterator<ObjectPair> iter = PAIR_LIST.iterator();
+		while(iter.hasNext()) {
+			ObjectPair ob = iter.next();
+			switch(ob.getId()) {
+			case 3595:
+			case 3599:
+			case 4503:
+			case 3596:
+				if(Double.isInfinite(ob.keywordScoreD)) {
+					System.out.println(ob.getSqlValues() + "////" + ob.keywordScoreD);
+				}
+			}
+		}	
+	}
+	
+	public static void printAuthorSize() {
+		System.out.println(AUTHOR_LIST.size());
 	}
 }
